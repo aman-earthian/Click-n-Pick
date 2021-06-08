bc72ca033bfe026e2b6bf5afc4bcb3de87f0c072
Aman Kumar Goyal <goyalaman2072001@gmail.com>
2021-06-08 11:34:22 +0530

    Added_Config

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI : "smongodb+srv://goyalaman:Ax9ZuLxKQ9aa2Y2@cluster0.72ume.mongodb.net/shopping?retryWrites=true&w=majority",
+    secretKey : "Anakin is darth vader"
+}
